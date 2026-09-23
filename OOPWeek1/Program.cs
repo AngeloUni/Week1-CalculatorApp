0ad2427d190fa3240fd55219f0b9c489d465d6ce
Angelo <Cuento_Raon-A@ulster.ac.uk>
2026-09-23 13:16:09 +0300

    Calculator App (Tasks done: Variables, User Inputs, Printing out result)

--- OOPWeek1/Program.cs
+++ OOPWeek1/Program.cs
@@ -1,12 +1,40 @@
-﻿// // Hello World
+﻿// Week 9: Tutorial 9 (Creating a simple calculator app)
+
+
+// // Hello World
 // Console.WriteLine("Hello, World!");
 
 
-// Variable
-int score = 4;
-Console.WriteLine("HI " + score);
-ConsoleKeyInfo keyy = Console.ReadKey();
-Console.WriteLine(keyy);
+// // Variable
+// int score = 4;
+// Console.WriteLine("HI " + score);
+// ConsoleKeyInfo keyy = Console.ReadKey();
+// Console.WriteLine(keyy);
 
-// Testing Github Push - Number 2
 
+
+
+
+// // ---!!!---!!!--- CALCULATOR APP ---!!!---!!!---
+
+// Calculator App - Variables
+// int firstNumber = 5;
+// int secondNumber = 20;
+//
+// int result = firstNumber + secondNumber;
+// Console.WriteLine("The addition of both numbers is {0}", result);
+// Console.ReadKey();
+
+// Calculator App - Accepting User Input
+
+Console.WriteLine("Type the first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Type the first number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber + secondNumber;
+
+Console.WriteLine("Adding {0} and {1} gives {2}", firstNumber, secondNumber, result);
+
+// // ---!!!---!!!--- CALCULATOR APP ---!!!---!!!---
