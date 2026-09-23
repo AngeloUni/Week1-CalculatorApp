331e6df5076d1ac5e51483fded6ff8920c955b7e
Angelo <Cuento_Raon-A@ulster.ac.uk>
2026-09-23 12:50:03 +0300

    Initial Commit

---
+++ OOPWeek1/Program.cs
@@ -0,0 +1,10 @@
+﻿// // Hello World
+// Console.WriteLine("Hello, World!");
+
+
+// Variable
+int score = 4;
+Console.WriteLine("HI " + score);
+ConsoleKeyInfo keyy = Console.ReadKey();
+Console.WriteLine(keyy);
+
